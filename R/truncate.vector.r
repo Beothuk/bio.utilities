b9eccbf807335a20663de82368c0037e7cb0ebae
Jae S Choi <choijaeseok@gmail.com>
2016-07-27 11:15:29 -0300

    add truncation method

--- R/truncate.vector.r
+++ R/truncate.vector.r
@@ -1,5 +1,4 @@
 #' @export
-
 truncate.vector = function( x, lower=-Inf, upper=Inf ) {
   #\\ truncate a vector or scalar to be bounded within a range
   .Internal( pmin( na.rm=TRUE, .Internal(pmax( na.rm=TRUE, x, lower ) ), upper ) )
