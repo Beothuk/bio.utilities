27462830e39133befd20bb235da27302624df942
BradHubley <brad.hubley@dfo-mpo.gc.ca>
2016-10-13 14:06:08 -0300

    geomean use median as offset

--- R/geomean.r
+++ R/geomean.r
@@ -4,6 +4,6 @@
 #' @author  unknown, \email{<unknown>@@dfo-mpo.gc.ca}
 #' @export
 geomean <- function(x){
-	x = exp(mean(log(x+median(x)))))-median(x)
+	x = exp(mean(log(x+median(x))))-median(x)
 	return(x)
 }
