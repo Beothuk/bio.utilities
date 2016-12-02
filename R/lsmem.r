d5caaf178bdd2731c681bd20e8b668cd0a1bccc7
Jae S Choi <choijaeseok@gmail.com>
2016-12-02 17:09:31 -0400

    add lsmem

--- R/lsmem.r
+++ R/lsmem.r
@@ -1,2 +1,8 @@
+#' @title lsmem
+#' @description list memory usage of user workspace
+#' @family abysmally documented
+#' @author jae, \email{jae.choi@dfo-mpo.gc.ca}
+#' @export
+
 lsmem = function() { as.matrix(sort( sapply(ls(envir=globalenv()),function(x){object.size(get(x))})) ) }
 
