bdd1d7a1d8fef3db39bff1239af49dd985b2c96d
MCassista-Da Ros <manon.cassistadaros@dfo-mpo.gc.ca>
2016-12-21 10:41:56 -0400

    Update to PBSmap

--- R/makePBS.r
+++ R/makePBS.r
@@ -11,6 +11,7 @@ makePBS <- function(x,polygon=T,projection="LL") {
 	if(any(names(x) %in% c('y','x'))) x <- rename.df(x,n0=c('x','y'),n1=c('X','Y'))
 	if(any(names(x) %in% c('LAT','LON'))) x <- rename.df(x,n0=c('LON','LAT'),n1=c('X','Y'))
 	if(any(names(x) %in% c('DDLAT','DDLON'))) x <- rename.df(x,n0=c('DDLON','DDLAT'),n1=c('X','Y'))
+	if(any(names(x) %in% c('LATITUDE','LONGITUDE'))) x <- rename.df(x,n0=c('LONGITUDE','LATITUDE'),n1=c('X','Y'))
 	
 	if(polygon) {
 			x$PID <- 1
