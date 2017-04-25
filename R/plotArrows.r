561175ae29985214b088b483ffc62de9975245b1
BradHubley <brad.hubley@dfo-mpo.gc.ca>
2017-04-25 15:47:57 -0300

    modified plot arrows to be customizable, have to specify arrows args

--- R/plotArrows.r
+++ R/plotArrows.r
@@ -1,5 +1,5 @@
 #' @export
-plotArrows = function (Y,X,add=F) {
+plotArrows = function (Y,X,add=F,colfun=rainbow,...) {
 
   #//trace arrows around x,y points from start to end
   xl = c(min(X),max(X))
@@ -7,11 +7,11 @@ plotArrows = function (Y,X,add=F) {
   if(Y[length(Y)]==0) {Y=Y[-length(Y)]}
   if(length(Y) !=length(X)) {X=X[-length(X)]}
   n = length(Y)
-  cl = rainbow(n)
+  cl = colfun(n)
   if(add==F) plot(1,1,ylim=yl,xlim=xl,type='n')
   
   for(i in 1:n) {
-    arrows(x0=X[i],x1=X[i+1],y0=Y[i],y1=Y[i+1],angle=45,length=0.1,lwd=2.5,col=cl[i])
+    arrows(x0=X[i],x1=X[i+1],y0=Y[i],y1=Y[i+1],angle=45,col=cl[i],...)
   }
   points(X,Y,pch=16,cex=1,col='black')
   points(X[1],Y[1],pch=17,cex=2,col='black')
