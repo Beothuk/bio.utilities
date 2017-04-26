b3b800838e565012f10218a821ff7fb2e99604e4
BradHubley <brad.hubley@dfo-mpo.gc.ca>
2017-04-26 10:15:19 -0300

    modify plotarrows

--- R/plotArrows.r
+++ R/plotArrows.r
@@ -1,5 +1,5 @@
 #' @export
-plotArrows = function (Y,X,add=F,colfun=rainbow,...) {
+plotArrows = function (Y,X,add=F,colfun=rainbow,startend=T,...) {
 
   #//trace arrows around x,y points from start to end
   xl = c(min(X),max(X))
@@ -14,6 +14,8 @@ plotArrows = function (Y,X,add=F,colfun=rainbow,...) {
     arrows(x0=X[i],x1=X[i+1],y0=Y[i],y1=Y[i+1],angle=45,col=cl[i],...)
   }
   points(X,Y,pch=16,cex=1,col='black')
-  points(X[1],Y[1],pch=17,cex=2,col='black')
-  points(X[n],Y[n],pch=17,cex=2,col='black')
+  if(startend){
+    points(X[1],Y[1],pch=17,cex=2,col='black')
+    points(X[n],Y[n],pch=17,cex=2,col='black')
+  }
 }
