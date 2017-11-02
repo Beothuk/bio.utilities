3cc71ee584a43c1c02fd242c2b6fb036630b6422
Jae S Choi <choijaeseok@gmail.com>
2017-11-02 16:26:04 -0300

    clean up dependencies

--- inst/examples/mapping.made.easy.r
+++ inst/examples/mapping.made.easy.r
@@ -16,29 +16,29 @@ bioLibrary("bio.utilities")
 bioLibrary("bio.bathymetry")
 
 
-m = map()   # low resolution map of the world .. default is rectangular coords
+m = maps::map()   # low resolution map of the world .. default is rectangular coords
 str(m)
 
 
 # dealing with projections in a simple manner: use "mapproj"
 graphics.off()
 require(mapproj)
-map(projection="polyconic")
+maps::map(projection="polyconic")
 
 graphics.off()
-map(projection="albers", par=c(30,40) )
+maps::map(projection="albers", par=c(30,40) )
 
 graphics.off()
-map(projection="mollweide" )
+maps::map(projection="mollweide" )
 
 graphics.off()
-map("worldHires", projection="gnomonic", orient=c(0,-100,0)) # example of orient
+maps::map("worldHires", projection="gnomonic", orient=c(0,-100,0)) # example of orient
 
 graphics.off()
-m = map("worldHires", plot=FALSE)
-map("worldHires", projection='orth',orient=c(41,-74,0))
+m = maps::map("worldHires", plot=FALSE)
+maps::map("worldHires", projection='orth',orient=c(41,-74,0))
 str(m)
-map.grid(m, col="lightgray", label=FALSE, lty="dotted")
+maps::map.grid(m, col="lightgray", label=FALSE, lty="dotted")
 
 # to add layers to a projected map, we need to deal with projections
 points(mapproject(list(y=42.5,x=-68)),col="red", pch="x", cex=3 )
@@ -50,35 +50,35 @@ points(mapproject(list(y=42.5,x=-68)),col="red", pch="x", cex=3 )
 
 
 graphics.off()
-map( "worldHires", projection="ortho", xlim=c(-165,-53), ylim=c(-0, 90 ),  )      # national boundaries
-map( "worldHires", projection="azequalarea", xlim=c(-165,-53), ylim=c(-0, 90 ),  )      # national boundaries
-map( "worldHires", projection="eisenlohr", xlim=c(-165,-53), ylim=c(-0, 90 ),  )      # national boundaries
+maps::map( "worldHires", projection="ortho", xlim=c(-165,-53), ylim=c(-0, 90 ),  )      # national boundaries
+maps::map( "worldHires", projection="azequalarea", xlim=c(-165,-53), ylim=c(-0, 90 ),  )      # national boundaries
+maps::map( "worldHires", projection="eisenlohr", xlim=c(-165,-53), ylim=c(-0, 90 ),  )      # national boundaries
 
 
 # Canada only
 graphics.off()
-map( "worldHires", regions="Canada", xlim=c(-141,-53), ylim=c(40,85), col="gray90", fill=TRUE)
+maps::map( "worldHires", regions="Canada", xlim=c(-141,-53), ylim=c(40,85), col="gray90", fill=TRUE)
 
 # oops forgot US
-map( "worldHires", regions=c("USA"), resolution=0, add=TRUE )
+maps::map( "worldHires", regions=c("USA"), resolution=0, add=TRUE )
 
 # or both together
 graphics.off()
-map( "worldHires", regions=c("Canada", "USA"), xlim=c(-141,-53), ylim=c(40,85), col="gray90", fill=TRUE)
+maps::map( "worldHires", regions=c("Canada", "USA"), xlim=c(-141,-53), ylim=c(40,85), col="gray90", fill=TRUE)
 
 
 # aside .. how to extract polygons
-map( "worldHires", regions=c("Canada", "USA"), xlim=c(-141,-53), ylim=c(40,85))
-mCan = map( "worldHires", regions="Canada", plot=FALSE, resolution=0)
+maps::map( "worldHires", regions=c("Canada", "USA"), xlim=c(-141,-53), ylim=c(40,85))
+mCan = maps::map( "worldHires", regions="Canada", plot=FALSE, resolution=0)
 lines( mCan,col="green" )
 
 
 # zoom in and use all data
 graphics.off()
-map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
+maps::map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
 
 # add some more things
-map.scale()
+maps::map.scale()
 title('Nova Scotia, High resolution')
 points( -60, 44, col="red", pch=21, cex=10 )
 text( -60, 44, "Sable Island" )
@@ -110,8 +110,8 @@ points( lat~lon, data=gs, bg=cols[zi], col=cols[zi], pch=21, cex=1.5 )
 
 # zoom in and use all data .. look at temperature as bubble plots
 graphics.off()
-map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
-map.scale()
+maps::map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
+maps::map.scale()
 title('Nova Scotia, High resolution')
 
 
@@ -133,8 +133,8 @@ points( lat~lon, data=gs,  bg=cols[zi], col=cols[zi], pch=21, cex=zcat[zi]/10 )
 # or to use transparencies for temperture in this case
 library(scales)  #for transparency
 graphics.off()
-map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
-map.scale()
+maps::map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
+maps::map.scale()
 title('Nova Scotia, High resolution')
 
 z = gs$bottom_temperature
@@ -160,14 +160,14 @@ points( lat~lon, data=gs,  bg=alpha(cols[zi], zalpha[zi] ), col=alpha(cols[zi], 
 ## to get some isobaths
 graphics.off()
 i1000 = isobath.db( p=p, DS="isobath", depths=c(1000 )
-map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
+maps::map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
 lines(i1000)
 
 ## to bring in some ARCinfo shape files:
 library(maptools)  #for shapefiles .. 100 m isobath
 fn = find.bio.gis( "dm100_region" )
 dm100 <- readShapePoly( fn)   #
-map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
+maps::map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=FALSE, resolution=0 )
 plot( dm100, add=TRUE )  # there seem to be some problems with the polygon data ... :)
 
 
@@ -192,7 +192,7 @@ print(pl)
 
 require(maps)
 require(mapdata)
-coast =  map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=TRUE, resolution=0, plot=FALSE)
+coast =  maps::map( "worldHires", regions=c("Canada", "USA"), xlim=c(-72, -56 ), ylim=c(42,49), fill=TRUE, resolution=0, plot=FALSE)
 
 
 trellis.par.set(col.whitebg())
